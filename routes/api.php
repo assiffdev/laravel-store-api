e26527218af882017f475016bb97bb6c5d4aaa32
assiffdev <assiffdev@gmail.com>
2025-05-26 16:10:28 +0500

    apiResouces and controller added for user and profile

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+use App\Http\Controllers\API\SuperAdmin\UserController;
+use App\Http\Controllers\API\StoreAdmin\UserController as StoreAdminUserController;
+//use App\Http\Controllers\API\StoreUser;
+use App\Http\Controllers\API\StoreUser\ProfileController as StoreUserProfileController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('super-admin')
+    ->middleware(['auth:sanctum', 'role:super-admin'])
+    ->group(function () {
+        Route::apiResource('users', UserController::class);
+});
 
-Route::middleware(['auth:sanctum'])->group(function () {
-      // Protected API routes
-  });
+Route::prefix('store-admin')
+    ->middleware(['auth:sanctum', 'role:store-admin'])
+    ->group(function () {
+        Route::apiResource('users', StoreAdminUserController::class);
+});
 
+Route::prefix('store-user')->middleware(['auth:sanctum', 'role:store-user'])->group(function () {
+    Route::get('profile', [StoreUserProfileController::class, 'show']);
+    Route::put('profile', [StoreUserProfileController::class, 'update']);
+});
