c13af8f8a524f0300c62dc1248f8b5c19fb6ba92
assiffdev <assiffdev@gmail.com>
2025-05-26 14:44:04 +0500

    migrations added

---
+++ database/migrations/2025_05_26_093214_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('stock')->default(0);
+            $table->foreignId('store_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+
+            $table->foreignId('category_id')
+                  ->constrained()
+                  ->onDelete('set null')
+                  ->nullable();
+
+            $table->boolean('is_active')->default(true);
+            $table->string('image_url')->nullable();
+
+            // SEO fields
+            $table->string('meta_title')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->string('og_title')->nullable();
+            $table->text('og_description')->nullable();
+            $table->string('og_image')->nullable();
+            $table->string('conaonical_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2025_05_26_094216_add_store_id_is_active_role_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('store_id')
+                  ->nullable()
+                  ->constrained()
+                  ->onDelete('set null');
+            $table->boolean('is_active')->default(true);
+            $table->string('role')->default('store_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
